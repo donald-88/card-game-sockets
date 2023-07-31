d8316da4ffe060d33aaa78aad5b14d53e2f84b2e
McDonald Namba <nambamcdonald@gmail.com>
2023-07-31 13:29:53 +0200

    fixed sign up

--- lib/pages/authentication/signUp.dart
+++ lib/pages/authentication/signUp.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import '../../utils/auth.dart';
 import '../../widgets/loading.dart';
 
 class SignUp extends StatefulWidget {
@@ -15,8 +16,18 @@ class _SignUpState extends State<SignUp> {
   final emailController = TextEditingController();
   final passwordController = TextEditingController();
 
+  final AuthService _auth = AuthService();
+
   void signUp() async {
     Loading(context);
+    dynamic result = _auth.registerWithEmailAndPassword(
+        emailController.text, passwordController.text);
+
+    if (result == null) {
+      Navigator.of(context).pop();
+    } else {
+      Navigator.of(context).pop();
+    }
   }
 
   @override
