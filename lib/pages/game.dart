885cf40ff40dc794e5dbde0a276dd928e844399c
McDonald Namba <nambamcdonald@gmail.com>
2023-08-05 13:30:21 +0200

    going home

--- lib/pages/game.dart
+++ lib/pages/game.dart
@@ -1,6 +1,9 @@
 import 'package:card_game_sockets/utils/deck.dart';
 import 'package:card_game_sockets/widgets/playingCard.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
+
+import '../models/roomModel.dart';
 
 class GamePage extends StatefulWidget {
   const GamePage({super.key});
@@ -15,16 +18,32 @@ class _GamePageState extends State<GamePage> {
     super.initState();
     initializeGame();
   }
+
+  late String roomId;
   List<PlayingCard> deck = [];
 
-  void initializeGame(){
+  void initializeGame() async{
     deck = buildDeck();
     deck.shuffle();
-    
+    for(var card in deck){
+      print("${card.suit} ${card.value}");
+    }
+    DatabaseReference roomRef = FirebaseDatabase.instance.ref().child('rooms').child(roomId);
+      final snapshot = await roomRef.get();
+      if (snapshot.exists) {
+        Map<String, dynamic> roomData = snapshot.value as Map<String, dynamic>;
+        RoomModel roomModel = RoomModel.fromJson(roomData);
+        for(var player in roomModel.players){
+          player['hand'
+        }
+      } else {
+        print('No data available.');
+      }
   }
 
   @override
   Widget build(BuildContext context) {  
+    roomId = ModalRoute.of(context)!.settings.arguments as String;
     return Scaffold(
         backgroundColor: Colors.green.shade800,
         floatingActionButton: FloatingActionButton(
